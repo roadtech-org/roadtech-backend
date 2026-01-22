d49e788dc2dd9eee63473256db8409d3f2c8c9e1
Abhay Kumar Vishwakarma <akraj6938@gmail.com>
2026-01-22 10:56:06 +0530

    Update web config

--- src/main/java/com/roadtech/config/WebConfig.java
+++ src/main/java/com/roadtech/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(
-                "http://localhost:5173",
+            .allowedOriginPatterns(
+                "http://localhost:*",
                 "https://roadtech.vercel.app",
                 "https://*.vercel.app"
             )
